56425ad1aa7266748bcbdb2311e4716d3fd0436f
kazi-mifta <kazimifta13@gmail.com>
2017-08-04 21:18:10 +0600

    added splash-screen Activity

--- app/src/main/java/com/campusapp/cuet/Contributors.java
+++ app/src/main/java/com/campusapp/cuet/Contributors.java
@@ -1,23 +1,77 @@
 package com.campusapp.cuet;
 
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import agency.tango.materialintro.R;
 
-/**
- * Created by Mifta on 5/14/2017.
- */
+public class Contributors extends Activity {
 
-public class Contributors extends AppCompatActivity {
-
-    TextView Title;
-    TextView Description;
+    private List<Person> persons;
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contributors);
+
+        setContentView(R.layout.recyclerview_activity);
+
+        rv=(RecyclerView)findViewById(R.id.rv);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+        initializeData();
+        initializeAdapter();
+    }
+
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+        persons.add(new Person("Unkonwn CUETIAN", "INSIDER BUild", R.drawable.cuet_logo));
+
+    }
+
+    private void initializeAdapter(){
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);
     }
 }
